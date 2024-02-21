7748723292c0a43fd45e9e20a47c5bf880fe7f24
Jeremy Yochum <jyochum391@yahoo.com>
2024-02-21 13:20:02 -0500

    update header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 export default function Header() {
 
     return (
-        <div>Hello</div>
+        <div className="header">
+            <h1 className="page-icon">Jokes</h1>
+            <img src="/transparent-exclamation.jpg" className="exclamation" />
+        </div>
     )
 }
