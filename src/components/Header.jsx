ed3368d3930c9c850a38bb328bc49dfea493e301
Jeremy Yochum <jyochum391@yahoo.com>
2024-02-21 19:56:56 -0500

    update exclamation mark image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@ export default function Header() {
 
     return (
         <div className="header">
-            <h1 className="page-icon">Jokes</h1>
-            <img src="/transparent-exclamation.jpg" className="exclamation" />
+            <div className="icon-wrapper">
+                <h1 className="page-icon">Jokes</h1>
+                <img src="/transparent-exclamation.png" className="exclamation" />
+            </div>
         </div>
     )
 }
