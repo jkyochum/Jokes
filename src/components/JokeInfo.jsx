86165944056cf7bac215c52ba5aa6b348703ec08
Jeremy Yochum <jyochum391@yahoo.com>
2024-02-21 20:08:02 -0500

    add form to JokeInfo.jsx

--- src/components/JokeInfo.jsx
+++ src/components/JokeInfo.jsx
@@ -1,6 +1,17 @@
 export default function JokeInfo() {
 
     return (
-        <div>joke Info</div>
+        <div className="main">
+            <form>
+                <label htmlFor="jokeSelector">What Kind of Joke?</label>
+                <select>
+                    <option>--Select--</option>
+                    <option>Classic Jokes</option>
+                    <option>Chuck Norris Jokes</option>
+                    <option>Dad Jokes</option>
+                </select>
+                <button>Get a Joke</button>
+            </form>
+        </div>
     )
 }
