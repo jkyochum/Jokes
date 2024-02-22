1833ad77f42d21a10894b0f7ac6f8547535eea02
Jeremy Yochum <jyochum391@yahoo.com>
2024-02-22 16:39:35 -0500

    add form / add form state object / add onChange event function

--- src/components/JokeInfo.jsx
+++ src/components/JokeInfo.jsx
@@ -1,14 +1,38 @@
+import { useState } from 'react'
+
 export default function JokeInfo() {
+    const [formInfo, setFormInfo] = useState(
+        {
+            "typeOfJoke": "classic"
+        }
+    )
+
+    console.log(formInfo)
+
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setFormInfo(() => {
+            return {
+                [name]: value
+            }
+        })
+    }
+
 
     return (
         <div className="main">
             <form>
-                <label htmlFor="jokeSelector">What Kind of Joke?</label>
-                <select>
-                    <option>--Select--</option>
-                    <option>Classic Jokes</option>
-                    <option>Chuck Norris Jokes</option>
-                    <option>Dad Jokes</option>
+                <label htmlFor="typeOfJoke">What Kind of Joke Would You Like?</label>
+                <select
+                    id="typeOfJoke"
+                    name="typeOfJoke"
+                    value={formInfo.typeOfJoke}
+                    onChange={handleChange}
+                >
+                    <option value="classic">Classic Joke</option>
+                    <option value="chuckNorris">Chuck Norris Joke</option>
+                    <option value="dad">Dad Joke</option>
                 </select>
                 <button>Get a Joke</button>
             </form>
